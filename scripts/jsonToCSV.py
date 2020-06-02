4f4dd1946d1b3e8b290674a3467724b1d3e20cf1
Sahil Bansal <shlbnsl843@gmail.com>
2020-06-02 09:21:50 +0530

    Update jsonToCSV script

--- scripts/jsonToCSV.py
+++ scripts/jsonToCSV.py
@@ -1,11 +1,14 @@
 import csv, json, sys
 
+rows = [["Entry No", "Marks"]]
 if sys.argv[1] is not None and sys.argv[2] is not None:
     fileInput = sys.argv[1]
     fileOutput = sys.argv[2]
     inputFile = open(fileInput) 
-    outputFile = open(fileOutput, 'w+') 
     data = eval(inputFile.read()) 
-    print(data)
-    print(data.keys())
-    
+    # print(data)
+    for key in data.keys():
+        rows.append([key, 1])
+    with open(fileOutput, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(rows)
